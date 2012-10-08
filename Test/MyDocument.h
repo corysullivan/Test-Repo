164497ca11ed64d4188b31758ee84da8762958d1
Cory Sullivan <cory.sullivan@me.com>
2012-10-08 19:19:52 -0300

    First Commit

---
+++ Test/MyDocument.h
@@ -0,0 +1,13 @@
+//
+//  MyDocument.h
+//  Test
+//
+//  Created by Cory Sullivan on 2012-10-08.
+//  Copyright (c) 2012 Cory Sullivan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSPersistentDocument
+
+@end
